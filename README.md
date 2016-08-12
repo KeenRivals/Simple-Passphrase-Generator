e3d83b82f8a150e797c75a474cb28b81611b46d3
KeenRivals <KeenRivals@users.noreply.github.com>
2016-08-12 12:51:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Passphrase Generator
+
+This is a simple passphrase generator which uses the [EFF's large wordlist](https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases).
---
+++ generate.js
@@ -0,0 +1,62 @@
+/* Generate passphrases from form. Called onload and when Generate is clicked. */
+function generate(form){
+	var numWords = form.numWords.value; /* Number of words per passphrase. */
+	var numPassphrases = form.numPassphrases.value; /* Number of passphrases. */
+	var wordlist = eff_large_wordlist; /* Loaded via script tag in index.html */
+	
+	/* Build a list of passphrases */
+	var passphraseList = [];
+	for (var i=0;i<numPassphrases;i++) {
+		passphraseList.push(buildPassphrase(wordlist,numWords));
+	}
+	
+	/* Send list of passphrases to the page or a download. */
+	if(form.display.value=="display"){
+		displayResults(passphraseList);
+	} else if( form.display.value=="download"){
+		downloadResults(passphraseList);
+	}
+}
+
+/* Create passphrase from a wordlist with the given number of words. */
+function buildPassphrase(wordlist, numWords){
+	var wordlistLength = wordlist.length;
+	var passphrase = "";
+	
+	for (var i=0;i<numWords;i++){
+		passphrase += wordlist[ Math.floor(Math.random() * wordlistLength) ] +" ";
+	}
+	
+	return passphrase.trim();
+}
+
+/* Add line breaks then inject the generated passphrases into #result */
+function displayResults(passphraseList){
+	var result = ""
+	
+	for(var i=0;i<passphraseList.length;i++){
+		result += passphraseList[i] + '<br>';
+	}
+	
+	document.getElementById("result").innerHTML = result;
+	
+}
+
+/* Send the list of passphrases to the browser as a download. */
+function downloadResults(passphraseList){
+	var result = "";
+	
+	/* Add dos line endings. Win notepad only understands dos. Others are smart. */
+	for (var i=0;i<passphraseList.length;i++){
+		result += passphraseList[i]+"\r\n";
+	}
+	
+	/* Create an a element with a data uri of the password list and click it. */
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(result));
+	element.setAttribute('download', 'Passphrase List.txt');
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
